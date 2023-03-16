ee1f2643e2226abf63e375fd71911c6c295adcf5
Tamar Al-Tahan <tamaraltahan@gmail.com>
2023-03-16 05:01:20 -0700

    cleaning backend. minor changes

--- backend/server.js
+++ backend/server.js
@@ -40,99 +40,49 @@ app.listen(port, () => {
   // console.log(`Example app listening on port ${port}`);
 });
 
+let userID;
+let playerData;
+
+app.post('/setUserID/:userid', (req, res) => {
+  userID = req.params.username;
+  playerData = require(`./python/PlayerEntries/${userID}.json`);
+  res.send(`User ID set as ${userID}`)
+})
+
 
 app.get('/players/:userid', (req, res) => {
-  const username = req.params.username;
-  const playerData = require(`./python/PlayerEntries/${username}.json`);
   res.json(playerData.playerData)
 });
 
 app.get('/outlierWinsPositive/:userid', (req, res) => {
-  const username = req.params.username;
-  const playerData = require(`./python/PlayerEntries/${username}.json`);
   res.json(playerData.outliers.outlierWinsPositive)
 });
 
 app.get('/outlierTotalsPositive/:userid', (req, res) => {
-  const username = req.params.username;
-  const playerData = require(`./python/PlayerEntries/${username}.json`);
   res.json(playerData.outliers.outlierTotalsPositive)
 });
 
 app.get('/outliersWinsNegative/:userid', (req, res) => {
-  const username = req.params.username;
-  const playerData = require(`./python/PlayerEntries/${username}.json`);
   res.json(playerData.outliers.outliersWinsNegative)
 });
 
 app.get('/outliersTotalNegative/:userid', (req, res) => {
-  const username = req.params.username;
-  const playerData = require(`./python/PlayerEntries/${username}.json`);
   res.json(playerData.outliers.outliersTotalNegative)
 });
 
 app.get('/highWinLowGames/:userid', (req, res) => {
-  const username = req.params.username;
-  const playerData = require(`./python/PlayerEntries/${username}.json`);
   res.json(playerData.outliers.outliersWinsNegative)
 });
 
 app.get('/lowWinLowGames/:userid', (req, res) => {
-  const username = req.params.username;
-  const playerData = require(`./python/PlayerEntries/${username}.json`);
   res.json(playerData.outliers.outliersTotalNegative)
 });
 
 app.get('/winPercentThreshold/:userid', (req, res) => {
-  const username = req.params.username;
-  const playerData = require(`./python/PlayerEntries/${username}.json`);
   res.json(playerData.outliers.winPercentThreshold)
 });
 
 app.get('/totalGamesThreshold/:userid', (req, res) => {
-  const username = req.params.username;
-  const playerData = require(`./python/PlayerEntries/${username}.json`);
   res.json(playerData.outliers.totalGamesThreshold)
 });
-
-
-// or I could do it as one giant block. I don't know what's a better sln here.
-
-/*
-
-app.get('/outliers/:userId/:subsection', (req, res) => {
-
-  let data = 
-
-  const userId = req.params.userId;
-  const subsection = req.params.subsection
-
-  const userOutlierWinsPositive = data.outliers.outlierWinsPositive[userId];
-  const userOutlierTotalsPositive = data.outliers.outlierTotalsPositive[userId];
-  const outliersWinsNegative = data.outliers.outliersWinsNegative[userId];
-  const outliersTotalNegative = data.outliers.outlierTotalsPositive[userId];
-  const highWinLowGames = data.outliers.highWinLowGames[userId];
-  const lowWinLowGames = data.outliers.lowWinLowGames[userId];
-  const winPercentThreshold = data.outliers.winPercentThreshold[userId];
-  const totalGamesThreshold = data.outliers.totalGamesThreshold[userId];
-
-  let userSubsection;
-  if (subsection === 'outlierWinsPositive') {
-    userSubsection = data.outliers.outlierWinsPositive[userId];
-  } else if (subsection === 'outlierTotalsPositive') {
-    userSubsection = data.outliers.outlierTotalsPositive[userId];
-  } else {
-    res.status(400).send('Invalid subsection');
-    return;
-  }
-
-  if (!userSubsection) {
-    res.status(404).send('User not found');
-    return;
-  }
-
-
-  res.send(userData);
-});
-*/
 
